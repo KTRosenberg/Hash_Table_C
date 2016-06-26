664571630e78bee69013bbf6b8fbe246b99b6bf9
KTRosenberg <ktr254@nyu.edu>
2016-06-26 19:22:18 -0400

    main2, test hash table with text file of words, hash table now dynamically allocates memory for an input char* key

--- main2.c
+++ main2.c
@@ -11,33 +11,44 @@ displays the table
 
 #include "hashtable.h"
 
+/*
+    main
+        Note: pass a single command line argument of 'p' to display the hash table
+*/
 int main(int argc, char *argv[])
 {
+    //open a text file (for reading), one word per line for now
     FILE *text = fopen("words.txt", "r");
     
     if(!text)exit(-1);
 
-    //default table size, low load factor of 0.2 for testing, default Jenkins hash function
+    //default table size, default load factor of 0.75, default Jenkins hash function
     hash_table_t* hash_table = init_hash_table(127151, DEFAULTMAXLOAD, DEFAULTHASH);
     if(!hash_table)exit(-1);
 
+    //count number of words reached
     uint64_t word_count = 1;
+    //create a buffer to store words of at most 50 characters
     char buffer[51];
-    char c = '\0';
+    //read each line
     while(fgets(buffer, 51, text))
     {
+        //replace the \n new line character with a c string null terminator 
         if(buffer[0] && buffer[strlen(buffer)-1] == '\n')
         {
             buffer[strlen(buffer)-1] = '\0';
         }
+        //also check for the \r new line character as part of \r\n Windows new line sequences
         if(buffer[0] && buffer[strlen(buffer)-1] == '\r')
         {
             buffer[strlen(buffer)-1] = '\0';
         }
-
+        //add the word and its word number to the hash table as a key-value pair
+        //(the put_record function allocates memory for a copy of the char*)
         put_record(buffer, word_count, hash_table);
         ++word_count;
     }
+    //close the text file
     fclose(text);
 
     if(ferror(text))
@@ -47,11 +58,14 @@ int main(int argc, char *argv[])
         exit(-2);
     }
     
+    //passing a single command line argument of 'p'
+    //has the program display the table
     if(argc == 2 && argv[1][0] == 'p')
     {
         print_table(hash_table);
     }
     
+    //delete the hash table
     del_hash_table(&hash_table);
 
     return 0;

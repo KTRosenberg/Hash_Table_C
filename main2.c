275d40bd1ce6b216fc9452aa2bb82cf82b89d928
KTRosenberg <ktr254@nyu.edu>
2016-06-26 21:13:50 -0400

    fixed misordering of error checks, placed additional error checks

--- main2.c
+++ main2.c
@@ -54,7 +54,7 @@ int main(int argc, char *argv[])
         ++word_count;
     }
 
-    if(ferror(text))
+    if(ferror(text) || !feof(text))
     {
         perror("ERROR");
         del_hash_table(&hash_table);

c1a62fe0ed7ac97fd8ef140fcc763fe01c0d9b8a
KTRosenberg <ktr254@nyu.edu>
2016-06-30 14:54:10 -0400

    building a basic testground, firstly to compare different ways of checking whether the table needs to be resized (and other details)

---
+++ main3.c
@@ -0,0 +1,86 @@
+/*
+Karl Toby Rosenberg
+
+main test program 2
+
+creates a hash table, reads a text file containing words,
+(handles UNIX and DOS end-lines), adds all words to the hash table,
+displays the table 
+
+*/
+
+#include "hashtable.h"
+
+/*
+    main
+        note: pass a single command line argument of 'x' to avoid displaying the hash table
+*/
+int main(int argc, char* argv[])
+{
+    //open a text file (for reading), one word per line for now
+    FILE *text = fopen("words.txt", "r");
+    
+    if(!text)exit(-1);
+
+    //number of test loops
+    uint64_t num_loops = 1000;
+    for(; num_loops != 0; --num_loops)
+    {
+        //default table size, default load factor of 0.75, default Jenkins hash function
+        hash_table_t* hash_table = init_hash_table(127189, DEFAULTMAXLOAD, DEFAULTHASH);
+        if(!hash_table)exit(-1);
+
+        //count number of words reached
+        uint64_t word_count = 1;
+        //create a buffer to store words of at most 50 characters
+        char buffer[51];
+        //read each line
+        while(fgets(buffer, 51, text))
+        {
+            //replace the \n new line character with a c string null terminator 
+            if(buffer[0] && buffer[strlen(buffer)-1] == '\n')
+            {
+                buffer[strlen(buffer)-1] = '\0';
+            }
+            //also check for the \r new line character as part of \r\n DOS/Windows new line sequences
+            if(buffer[0] && buffer[strlen(buffer)-1] == '\r')
+            {
+                buffer[strlen(buffer)-1] = '\0';
+            }
+            //add the word and its word number to the hash table as a key-value pair
+            //(the put_record function allocates memory for a copy of the char*)
+            if(!put_record(buffer, word_count, hash_table))
+            {
+                printf("%s\n", "hash table put error");
+                del_hash_table(&hash_table);
+                exit(-1);
+            }
+            ++word_count;
+        }
+
+        if(ferror(text) || !feof(text))
+        {
+            perror("ERROR");
+            del_hash_table(&hash_table);
+            fclose(text);
+            exit(-2);
+        }
+
+        //passing a single command line argument of 'x'
+        //avoids displaying the table
+        if(argc != 2 || argv[1][0] != 'x')
+        {
+            print_table(hash_table);
+        }
+    
+        //delete the hash table
+        del_hash_table(&hash_table);
+
+        rewind(text);
+    }
+
+    //close the text file
+    fclose(text);
+
+    return 0;
+}
--- makefile
+++ makefile
@@ -2,11 +2,16 @@ CC     = gcc
 CFLAGS = -O3 -g -Wall -Wextra
 OBJS = hashtable.o hashchain.o main1.o
 OBJS2 = hashtable.o hashchain.o main2.o
+OBJS3 = hashtable.o hashchain.o main3.o
+
 test1 : $(OBJS)
 	$(CC) $(CFLAGS) $(OBJS) -o test1
 
 test2 : $(OBJS2)
 	$(CC) $(CFLAGS) $(OBJS2) -o test2
+
+test3 : $(OBJS3)
+	$(CC) $(CFLAGS) $(OBJS3) -o test3
 
 hashtable.o : hashtable.c hashtable.h
 	$(CC) $(CFLAGS) -c hashtable.c
@@ -19,6 +24,9 @@ main1.o : main1.c hashtable.h
 
 main2.o : main2.c hashtable.h
 	$(CC) -c main2.c
+
+main3.o : main3.c hashtable.h
+	$(CC) -c main3.c
 
 clean :
 	-rm -f *.o *.core
